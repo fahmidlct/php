70745f1f0050d71251f91468d544a9f0d2cb98ad
fahmidlct <1994dotfm@gmail.com>
2020-02-06 09:24:43 +0700

    Edit variabel

--- fungsi_hari_to_minggu.php
+++ fungsi_hari_to_minggu.php
@@ -1,22 +1,24 @@
 <?php
 
 function to_week($hari = '') {
-	if ($hari < 1 || $hari == '') {
-		return false;
-	}
-			
-	if ($hari < 7) {
-		return $hari . " hari";
-	} else {
-		$a = $hari % 7;
-		$b = $hari / 7;
 
-		if ($a == 0) {
-			return floor($b) . " minggu";
-		} else {
-			return floor($b) . " minggu dan " . $a . " hari";
-		}      
-	}
+  if ($hari < 1 || $hari == '') {
+    return false;
+  }
+			
+  if ($hari < 7) {
+    return $hari . " hari";
+  } else {
+    $jml_hari = $hari % 7;
+    $jml_minggu = $hari / 7;
+
+    if ($jml_hari == 0) {
+      return floor($jml_minggu) . " minggu";
+    } else {
+      return floor($jml_minggu) . " minggu dan " . $jml_hari . " hari";
+    }      
+  }
+  
 }
 
 ?>
