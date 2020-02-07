1910f8776a65995eba42ee6778274ae14bd1bd03
fahmidlct <1994dotfm@gmail.com>
2020-02-07 08:23:39 +0700

    Revisi fungsi date_to_week

--- fungsi_php/fungsi_hari_to_minggu.php
+++ fungsi_php/fungsi_hari_to_minggu.php
@@ -3,22 +3,22 @@
 function to_week($hari = '') {
 
   if ($hari < 1 || $hari == '') {
-    return false;
+    $hasil =  false;
   }
 			
   if ($hari < 7) {
-    return $hari . " hari";
+    $hasil =  $hari . " hari";
   } else {
     $jml_hari = $hari % 7;
     $jml_minggu = $hari / 7;
 
     if ($jml_hari == 0) {
-      return floor($jml_minggu) . " minggu";
+      $hasil =  floor($jml_minggu) . " minggu";
     } else {
-      return floor($jml_minggu) . " minggu dan " . $jml_hari . " hari";
+      $hasil =  floor($jml_minggu) . " minggu dan " . $jml_hari . " hari";
     }      
   }
-  
+  return $hasil;
 }
 
 
--- json/index.php
+++ json/index.php
@@ -22,7 +22,15 @@ for ($i = 0; $i <10; $i++) {
 	//jangan lupa tambahkan indeks [$i]
 	$data['pegawai'][$i]['nik'] = $i;
 }
+
+$arr = json_encode($data);
 		
-echo json_encode($data);
+echo $arr;
+
+foreach ($data['pegawai'] as $k => $v) {
+	echo  $v['nik'] . "<br>";
+}
+
+
 
 //hasil {"status":1,"pegawai":[{"nik":0},{"nik":1},{"nik":2},{"nik":3},{"nik":4},{"nik":5},{"nik":6},{"nik":7},{"nik":8},{"nik":9}]}
